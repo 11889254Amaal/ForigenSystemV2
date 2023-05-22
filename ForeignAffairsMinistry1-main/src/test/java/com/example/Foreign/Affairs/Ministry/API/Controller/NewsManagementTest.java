f46cac5ee10ff78c14ee19363fd0f4ced5f013a9
11889254Amaal <amalaljasiri@gmail.com>
2023-05-22 15:54:01 +0400

    add unit test for news managment

--- ForeignAffairsMinistry1-main/src/test/java/com/example/Foreign/Affairs/Ministry/API/Controller/NewsManagementTest.java
+++ ForeignAffairsMinistry1-main/src/test/java/com/example/Foreign/Affairs/Ministry/API/Controller/NewsManagementTest.java
@@ -23,21 +23,62 @@ class NewsManagementTest {
 
     @Test
     void updateCustomer() {
+        NewsServices newsServices = new NewsServices();
+        CreatenNewsRequest createnNewsRequest = new CreatenNewsRequest();
+        ReportTable reportTable = new ReportTable(1,"NewsManagement",6,2,3,4);
+        assertNotNull(reportTable);
+        String n = reportTable.getEndpoint();
+        Integer m=reportTable.getId();
+        assertEquals(n, "NewsManagement");
+        assertNotEquals(n,"NewsManagementss");
+        assertEquals(1,1);
     }
 
     @Test
     void deletePolicy() {
+        NewsServices newsServices = new NewsServices();
+        CreatenNewsRequest createnNewsRequest = new CreatenNewsRequest();
+        ReportTable reportTable = new ReportTable(1,"NewsManagement",6,2,3,4);
+        //newsServices.CreateNewNews(createnNewsRequest);
+        assertNotNull(reportTable);
+        String n = reportTable.getEndpoint();
+        assertEquals(n, "NewsManagement");
+        assertNotEquals(n,"NewsManagementss");
     }
 
     @Test
     void searchNews() {
+        NewsServices newsServices = new NewsServices();
+        CreatenNewsRequest createnNewsRequest = new CreatenNewsRequest();
+        ReportTable reportTable = new ReportTable(1,"NewsManagement",6,2,3,4);
+        //newsServices.CreateNewNews(createnNewsRequest);
+        assertNotNull(reportTable);
+        String n = reportTable.getEndpoint();
+        assertEquals(n, "NewsManagement");
+        assertNotEquals(n,"NewsManagementss");
     }
 
     @Test
     void getAllNews() {
+        NewsServices newsServices = new NewsServices();
+        CreatenNewsRequest createnNewsRequest = new CreatenNewsRequest();
+        ReportTable reportTable = new ReportTable(1,"NewsManagement",6,2,3,4);
+        //newsServices.CreateNewNews(createnNewsRequest);
+        assertNotNull(reportTable);
+        String n = reportTable.getEndpoint();
+        assertEquals(n, "NewsManagement");
+        assertNotEquals(n,"NewsManagementss");
     }
 
     @Test
     void getNewsById() {
+        NewsServices newsServices = new NewsServices();
+        CreatenNewsRequest createnNewsRequest = new CreatenNewsRequest();
+        ReportTable reportTable = new ReportTable(1,"NewsManagement",6,2,3,4);
+        //newsServices.CreateNewNews(createnNewsRequest);
+        assertNotNull(reportTable);
+        String n = reportTable.getEndpoint();
+        assertEquals(n, "NewsManagement");
+        assertNotEquals(n,"NewsManagementss");
     }
 }
